154e7d8214d047de329145e97af4bfa57690c362
Dante Del Favero <delfavero.dante@volta.ts.it>
2023-02-07 16:17:30 +0100

    semaforo con il pulsante

--- input_pullup/input_pullup.ino
+++ input_pullup/input_pullup.ino
@@ -1,19 +1,47 @@
 
 const int buttonPin = 2;
-const int ledPin = 12;
+const int ledRedPin = 12;
+const int ledYellowPin = 11;
+const int ledGreenPin = 10;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(ledPin, OUTPUT);
+  pinMode(ledRedPin, OUTPUT);
+  pinMode(ledYellowPin, OUTPUT);
+  pinMode(ledGreenPin, OUTPUT);
   pinMode(buttonPin, INPUT_PULLUP);
 
+  digitalWrite(ledRedPin, HIGH);
+  digitalWrite(ledYellowPin, LOW);
+  digitalWrite(ledGreenPin, LOW);
+
+  // utilizzo la porta seriale ed imposto la velocità
+  Serial.begin(115200);
+
 }
+
+bool isStarted = false;
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  int buttonState = digitalRead(buttonPin); // spingo il bottone, leggo 0, lo lascio viene 1
-  digitalWrite(ledPin, !buttonState); // da 0 diventa 1, quindi ALTO
-
-
+  int buttonState = digitalRead(buttonPin);
+  if (buttonState == LOW)
+    isStarted = !isStarted;
+  Serial.println(isStarted);
+  if (isStarted == true) {
+    digitalWrite(ledRedPin, LOW);
+    digitalWrite(ledYellowPin, LOW);
+    digitalWrite(ledGreenPin, HIGH);
+    delay(500);
+    digitalWrite(ledRedPin, LOW);
+    digitalWrite(ledYellowPin, HIGH);
+    digitalWrite(ledGreenPin, LOW);
+    delay(500);
+    digitalWrite(ledRedPin, HIGH);
+    digitalWrite(ledYellowPin, LOW);
+    digitalWrite(ledGreenPin, LOW);
+    delay(500);
+  } else
+    delay(500);
 }
